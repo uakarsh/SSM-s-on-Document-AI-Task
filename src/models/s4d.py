4afc7d75fa82be5d598f0d78194f505d7992da6e
uakarsh <55104596+uakarsh@users.noreply.github.com>
2023-05-23 15:13:03 +0530

    Added the `s4dfortokenclassification`

--- src/models/s4d.py
+++ src/models/s4d.py
@@ -1,10 +1,12 @@
-## Entire reference from https://github.com/HazyResearch/state-spaces/blob/main/models/s4/s4d.py
+# Entire reference from https://github.com/HazyResearch/state-spaces/blob/main/models/s4/s4d.py
 
 import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange, repeat
+from transformers.modeling_outputs import SequenceClassifierOutput
+
 
 class DropoutNd(nn.Module):
     def __init__(self, p: float = 0.5, tie=True, transposed=True):
@@ -13,7 +15,8 @@ class DropoutNd(nn.Module):
         """
         super().__init__()
         if p < 0 or p >= 1:
-            raise ValueError("dropout probability has to be in [0, 1), " "but got {}".format(p))
+            raise ValueError(
+                "dropout probability has to be in [0, 1), " "but got {}".format(p))
         self.p = p
         self.tie = tie
         self.transposed = transposed
@@ -22,13 +25,15 @@ class DropoutNd(nn.Module):
     def forward(self, X):
         """X: (batch, dim, lengths...)."""
         if self.training:
-            if not self.transposed: X = rearrange(X, 'b ... d -> b d ...')
+            if not self.transposed:
+                X = rearrange(X, 'b ... d -> b d ...')
             # binomial = torch.distributions.binomial.Binomial(probs=1-self.p) # This is incredibly slow because of CPU -> GPU copying
             mask_shape = X.shape[:2] + (1,)*(X.ndim-2) if self.tie else X.shape
             # mask = self.binomial.sample(mask_shape)
             mask = torch.rand(*mask_shape, device=X.device) < 1.-self.p
             X = X * mask * (1.0/(1-self.p))
-            if not self.transposed: X = rearrange(X, 'b d ... -> b ... d')
+            if not self.transposed:
+                X = rearrange(X, 'b d ... -> b ... d')
             return X
         return X
 
@@ -59,13 +64,13 @@ class S4DKernel(nn.Module):
         """
 
         # Materialize parameters
-        dt = torch.exp(self.log_dt) # (H)
-        C = torch.view_as_complex(self.C) # (H N)
-        A = -torch.exp(self.log_A_real) + 1j * self.A_imag # (H N)
+        dt = torch.exp(self.log_dt)  # (H)
+        C = torch.view_as_complex(self.C)  # (H N)
+        A = -torch.exp(self.log_A_real) + 1j * self.A_imag  # (H N)
 
         # Vandermonde multiplication
         dtA = A * dt.unsqueeze(-1)  # (H N)
-        K = dtA.unsqueeze(-1) * torch.arange(L, device=A.device) # (H N L)
+        K = dtA.unsqueeze(-1) * torch.arange(L, device=A.device)  # (H N L)
         C = C * (torch.exp(dtA)-1.) / A
         K = 2 * torch.einsum('hn, hnl -> hl', C, torch.exp(K)).real
 
@@ -80,53 +85,81 @@ class S4DKernel(nn.Module):
             self.register_parameter(name, nn.Parameter(tensor))
 
             optim = {"weight_decay": 0.0}
-            if lr is not None: optim["lr"] = lr
+            if lr is not None:
+                optim["lr"] = lr
             setattr(getattr(self, name), "_optim", optim)
 
 
-class S4D(nn.Module):
-    def __init__(self, d_model, d_state=64, dropout=0.0, transposed=True, **kernel_args):
+# Ref: https://github.com/HazyResearch/state-spaces/blob/main/example.py
+class S4ModelForTokenClassification(nn.Module):
+    def __init__(self, config):
+        '''
+        Args:
+            config: S4Config
+        '''
         super().__init__()
 
-        self.h = d_model
-        self.n = d_state
-        self.d_output = self.h
-        self.transposed = transposed
+        self.prenorm = config.prenorm
+        self.emb = nn.Embedding(
+            config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
 
-        self.D = nn.Parameter(torch.randn(self.h))
+        # Stack S4 layers as residual blocks
+        self.s4_layers = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        self.dropouts = nn.ModuleList()
 
-        # SSM Kernel
-        self.kernel = S4DKernel(self.h, N=self.n, **kernel_args)
-
-        # Pointwise
-        self.activation = nn.GELU()
-        # dropout_fn = nn.Dropout2d # NOTE: bugged in PyTorch 1.11
         dropout_fn = DropoutNd
-        self.dropout = dropout_fn(dropout) if dropout > 0.0 else nn.Identity()
 
-        # position-wise output transform to mix features
-        self.output_linear = nn.Sequential(
-            nn.Conv1d(self.h, 2*self.h, kernel_size=1),
-            nn.GLU(dim=-2),
+        for _ in range(n_layers):
+            self.s4_layers.append(
+                S4D(config.hidden_size, dropout=config.hidden_dropout_prob, transposed=True, lr=min(0.001, lr))
+            )
+            self.norms.append(nn.LayerNorm(config.hidden_size))
+            self.dropouts.append(dropout_fn(config.hidden_dropout_prob))
+
+        # Linear decoder
+        self.decoder = nn.Linear(config.hidden_size, config.num_classes)
+
+    def forward(self, input_ids, labels=None, **kwargs):
+        """
+        Args:
+            input_ids: (B, L) where L is the sequence length
+            labels: (B, L) where L is the sequence length
+        Returns:
+            logits: (B, L, d_output)
+            loss: scalar
+        """
+        x = self.emb(input_ids)  # (B, L, d_input) -> (B, L, d_model)
+        x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
+
+        for layer, norm, dropout in zip(self.s4_layers, self.norms, self.dropouts):
+            # Each iteration of this loop will map (B, d_model, L) -> (B, d_model, L)
+            z = x
+            if self.prenorm:
+                # Prenorm
+                z = norm(z.transpose(-1, -2)).transpose(-1, -2)
+
+            # Apply S4 block: we ignore the state input and output
+            z, _ = layer(z)
+            # Dropout on the output of the S4 block
+            z = dropout(z)
+            # Residual connection
+            x = z + x
+            if not self.prenorm:
+                # Postnorm
+                x = norm(x.transpose(-1, -2)).transpose(-1, -2)
+
+        x = x.transpose(-1, -2)
+
+        # Decode the outputs
+        logits = self.decoder(x)  # (B, L, d_model) -> (B, L, d_output)
+
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.d_output), labels.view(-1))
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits
         )
-
-    def forward(self, u, **kwargs): # absorbs return_output and transformer src mask
-        """ Input and output shape (B, H, L) """
-        if not self.transposed: u = u.transpose(-1, -2)
-        L = u.size(-1)
-
-        # Compute SSM Kernel
-        k = self.kernel(L=L) # (H L)
-
-        # Convolution
-        k_f = torch.fft.rfft(k, n=2*L) # (H L)
-        u_f = torch.fft.rfft(u, n=2*L) # (B H L)
-        y = torch.fft.irfft(u_f*k_f, n=2*L)[..., :L] # (B H L)
-
-        # Compute D term in state space equation - essentially a skip connection
-        y = y + u * self.D.unsqueeze(-1)
-
-        y = self.dropout(self.activation(y))
-        y = self.output_linear(y)
-        if not self.transposed: y = y.transpose(-1, -2)
-        return y, None # Return a dummy state to satisfy this repo's interface, but this can be modified
